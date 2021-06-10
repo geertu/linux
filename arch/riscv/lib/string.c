45e2002af575e7779e96b6f0ac9cd8a9d35872c8
Matteo Croce <mcroce@microsoft.com>
2021-06-10 03:58:05 +0200

    riscv: optimized memcpy
    Write a C version of memcpy() which uses the biggest data size allowed,
    without generating unaligned accesses.
    
    The procedure is made of three steps:
    First copy data one byte at time until the destination buffer is aligned
    to a long boundary.
    Then copy the data one long at time shifting the current and the next u8
    to compose a long at every cycle.
    Finally, copy the remainder one byte at time.
    
    On a BeagleV, the TCP RX throughput increased by 45%:
    
    before:
    
    $ iperf3 -c beaglev
    Connecting to host beaglev, port 5201
    [  5] local 192.168.85.6 port 44840 connected to 192.168.85.48 port 5201
    [ ID] Interval           Transfer     Bitrate         Retr  Cwnd
    [  5]   0.00-1.00   sec  76.4 MBytes   641 Mbits/sec   27    624 KBytes
    [  5]   1.00-2.00   sec  72.5 MBytes   608 Mbits/sec    0    708 KBytes
    [  5]   2.00-3.00   sec  73.8 MBytes   619 Mbits/sec   10    451 KBytes
    [  5]   3.00-4.00   sec  72.5 MBytes   608 Mbits/sec    0    564 KBytes
    [  5]   4.00-5.00   sec  73.8 MBytes   619 Mbits/sec    0    658 KBytes
    [  5]   5.00-6.00   sec  73.8 MBytes   619 Mbits/sec   14    522 KBytes
    [  5]   6.00-7.00   sec  73.8 MBytes   619 Mbits/sec    0    621 KBytes
    [  5]   7.00-8.00   sec  72.5 MBytes   608 Mbits/sec    0    706 KBytes
    [  5]   8.00-9.00   sec  73.8 MBytes   619 Mbits/sec   20    580 KBytes
    [  5]   9.00-10.00  sec  73.8 MBytes   619 Mbits/sec    0    672 KBytes
    - - - - - - - - - - - - - - - - - - - - - - - - -
    [ ID] Interval           Transfer     Bitrate         Retr
    [  5]   0.00-10.00  sec   736 MBytes   618 Mbits/sec   71             sender
    [  5]   0.00-10.01  sec   733 MBytes   615 Mbits/sec                  receiver
    
    after:
    
    $ iperf3 -c beaglev
    Connecting to host beaglev, port 5201
    [  5] local 192.168.85.6 port 44864 connected to 192.168.85.48 port 5201
    [ ID] Interval           Transfer     Bitrate         Retr  Cwnd
    [  5]   0.00-1.00   sec   109 MBytes   912 Mbits/sec   48    559 KBytes
    [  5]   1.00-2.00   sec   108 MBytes   902 Mbits/sec    0    690 KBytes
    [  5]   2.00-3.00   sec   106 MBytes   891 Mbits/sec   36    396 KBytes
    [  5]   3.00-4.00   sec   108 MBytes   902 Mbits/sec    0    567 KBytes
    [  5]   4.00-5.00   sec   106 MBytes   891 Mbits/sec    0    699 KBytes
    [  5]   5.00-6.00   sec   106 MBytes   891 Mbits/sec   32    414 KBytes
    [  5]   6.00-7.00   sec   106 MBytes   891 Mbits/sec    0    583 KBytes
    [  5]   7.00-8.00   sec   106 MBytes   891 Mbits/sec    0    708 KBytes
    [  5]   8.00-9.00   sec   106 MBytes   891 Mbits/sec   28    433 KBytes
    [  5]   9.00-10.00  sec   108 MBytes   902 Mbits/sec    0    591 KBytes
    - - - - - - - - - - - - - - - - - - - - - - - - -
    [ ID] Interval           Transfer     Bitrate         Retr
    [  5]   0.00-10.00  sec  1.04 GBytes   897 Mbits/sec  144             sender
    [  5]   0.00-10.01  sec  1.04 GBytes   894 Mbits/sec                  receiver
    
    And the decreased CPU time of the memcpy() is observable with perf top.
    This is the `perf top -Ue task-clock` output when doing the test:
    
    before:
    
    Overhead  Shared O  Symbol
      42.22%  [kernel]  [k] memcpy
      35.00%  [kernel]  [k] __asm_copy_to_user
       3.50%  [kernel]  [k] sifive_l2_flush64_range
       2.30%  [kernel]  [k] stmmac_napi_poll_rx
       1.11%  [kernel]  [k] memset
    
    after:
    
    Overhead  Shared O  Symbol
      45.69%  [kernel]  [k] __asm_copy_to_user
      29.06%  [kernel]  [k] memcpy
       4.09%  [kernel]  [k] sifive_l2_flush64_range
       2.77%  [kernel]  [k] stmmac_napi_poll_rx
       1.24%  [kernel]  [k] memset
    
    Signed-off-by: Matteo Croce <mcroce@microsoft.com>

---
+++ arch/riscv/lib/string.c
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * String functions optimized for hardware which doesn't
+ * handle unaligned memory accesses efficiently.
+ *
+ * Copyright (C) 2021 Matteo Croce
+ */
+
+#include <linux/types.h>
+#include <linux/export.h>
+
+/* Minimum size for a word copy to be convenient */
+#define MIN_THRESHOLD (BITS_PER_LONG / 8 * 2)
+
+/* convenience union to avoid cast between different pointer types */
+union types {
+	u8 *as_u8;
+	unsigned long *as_ulong;
+	uintptr_t as_uptr;
+};
+
+union const_types {
+	const u8 *as_u8;
+	const unsigned long *as_ulong;
+	uintptr_t as_uptr;
+};
+
+static const unsigned int bytes_long = sizeof(long);
+static const unsigned int mask = bytes_long - 1;
+
+void *__memcpy(void *dest, const void *src, size_t count)
+{
+	union const_types s = { .as_u8 = src };
+	union types d = { .as_u8 = dest };
+	int distance = 0;
+
+	if (count < MIN_THRESHOLD)
+		goto copy_remainder;
+
+	if (!IS_ENABLED(CONFIG_HAVE_EFFICIENT_UNALIGNED_ACCESS)) {
+		/* Copy a byte at time until destination is aligned. */
+		for (; d.as_uptr & mask; count--)
+			*d.as_u8++ = *s.as_u8++;
+
+		distance = s.as_uptr & mask;
+	}
+
+	if (distance) {
+		unsigned long last, next;
+
+		/*
+		 * s is distance bytes ahead of d, and d just reached
+		 * the alignment boundary. Move s backward to word align it
+		 * and shift data to compensate for distance, in order to do
+		 * word-by-word copy.
+		 */
+		s.as_u8 -= distance;
+
+		/*
+		 * Word-by-word copy by shifting data.
+		 * Works only on Little Endian machines.
+		 */
+		next = s.as_ulong[0];
+		for (; count >= bytes_long + mask; count -= bytes_long) {
+			last = next;
+			next = s.as_ulong[1];
+
+			d.as_ulong[0] = last >> (distance * 8) |
+					next << ((bytes_long - distance) * 8);
+
+			d.as_ulong++;
+			s.as_ulong++;
+		}
+
+		/* Restore s with the original offset. */
+		s.as_u8 += distance;
+	} else {
+		/*
+		 * If the source and dest lower bits are the same, do a simple
+		 * 32/64 bit wide copy.
+		 */
+		for (; count >= bytes_long; count -= bytes_long)
+			*d.as_ulong++ = *s.as_ulong++;
+	}
+
+copy_remainder:
+	while (count--)
+		*d.as_u8++ = *s.as_u8++;
+
+	return dest;
+}
+EXPORT_SYMBOL(__memcpy);
+
+void *memcpy(void *dest, const void *src, size_t count) __weak __alias(__memcpy);
+EXPORT_SYMBOL(memcpy);
